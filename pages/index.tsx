6b731ea255b74029b063fd6ae53eb4cf38879883
Rifat Hossain <rifat@workspaceit.com>
2021-10-11 08:37:58 +0600

    finish setting up husky for sol files

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { HiChevronDown, HiChevronUp } from 'react-icons/hi';
 
 const Home: NextPage = () => {
   return (
-    <div className="prose prose-sm lg:prose-lg mx-auto p-4">
+    <div className="p-4 mx-auto prose-sm prose lg:prose-lg">
       <h1>This is header</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat velit,
@@ -28,15 +28,15 @@ const Home: NextPage = () => {
         >
           Price
         </label>
-        <div className="mt-1 relative rounded-md shadow-sm">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <div className="relative mt-1 rounded-md shadow-sm">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <span className="text-gray-500 sm:text-sm">$</span>
           </div>
           <input
             type="text"
             name="price"
             id="price"
-            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+            className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
             placeholder="0.00"
           />
           <div className="absolute inset-y-0 right-0 flex items-center">
@@ -46,7 +46,7 @@ const Home: NextPage = () => {
             <select
               id="currency"
               name="currency"
-              className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+              className="h-full py-0 pl-2 text-gray-500 bg-transparent border-transparent rounded-md focus:ring-indigo-500 focus:border-indigo-500 pr-7 sm:text-sm"
             >
               <option>USD</option>
               <option>CAD</option>
