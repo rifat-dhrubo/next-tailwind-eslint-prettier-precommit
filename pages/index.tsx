6fb2e3ff5bc6f0a00f8f261a88800cb77d54df30
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2021-11-30 20:27:08 +0600

    base style and select added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import { HiChevronDown, HiChevronUp } from 'react-icons/hi';
+import { MultiSelect } from 'react-multi-select-component';
+import Select, { components } from 'react-select';
+
+const Input = (props: any) => (
+  <components.Input
+    {...props}
+    inputClassName="outline-none border-none shadow-none focus:ring-transparent"
+  />
+);
+
+const options = [
+  { label: 'Grapes 🍇', value: 'grapes' },
+  { label: 'Mango 🥭', value: 'mango' },
+  { label: 'Strawberry 🍓', value: 'strawberry' },
+  { label: 'Watermelon 🍉', value: 'watermelon' },
+  { label: 'Pear 🍐', value: 'pear', disabled: true },
+  { label: 'Apple 🍎', value: 'apple' },
+  { label: 'Tangerine 🍊', value: 'tangerine' },
+  { label: 'Pineapple 🍍', value: 'pineapple' },
+  { label: 'Peach 🍑', value: 'peach' },
+];
 
 const Home: NextPage = () => {
+  const [selected, setSelected] = useState([]);
   return (
-    <div className="prose prose-sm lg:prose-lg mx-auto p-4">
-      <h1>This is header</h1>
+    <div className="p-4 mx-auto">
+      <h1 className="mb-12 text-4xl font-bold text-gray-700">This is header</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat velit,
         sequi dignissimos reiciendis a iusto, voluptatem quasi quod quaerat
@@ -28,15 +51,15 @@ const Home: NextPage = () => {
         >
           Price
         </label>
-        <div className="mt-1 relative rounded-md shadow-sm">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <div className="relative mt-1 rounded-md shadow-sm">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <span className="text-gray-500 sm:text-sm">$</span>
           </div>
           <input
             type="text"
             name="price"
             id="price"
-            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+            className="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 pl-7 sm:text-sm"
             placeholder="0.00"
           />
           <div className="absolute inset-y-0 right-0 flex items-center">
@@ -46,7 +69,7 @@ const Home: NextPage = () => {
             <select
               id="currency"
               name="currency"
-              className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md"
+              className="h-full py-0 pl-2 text-gray-500 bg-transparent border-transparent rounded-md focus:ring-indigo-500 focus:border-indigo-500 pr-7 sm:text-sm"
             >
               <option>USD</option>
               <option>CAD</option>
@@ -54,6 +77,17 @@ const Home: NextPage = () => {
             </select>
           </div>
         </div>
+      </div>
+      <Select className="border-red-700" components={{ Input }} />
+      <div>
+        <h1>Select Fruits</h1>
+        <pre>{JSON.stringify(selected)}</pre>
+        <MultiSelect
+          options={options}
+          value={selected}
+          onChange={setSelected}
+          labelledBy={'Select'}
+        />
       </div>
     </div>
   );
